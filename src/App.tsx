ac635511814ef9a29bbe1ffa2819538d159e8882
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2023-01-01 12:39:28 +0300

    read localstorage on launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { habit } from './objects/objects';
 
 function App() {
+  const [appState, setAppState] = useState<habit[]>([]);
+
+  useEffect(() => {
+    setAppState(JSON.parse(localStorage.getItem('habits') || '[]'));
+  }, []);
+
   return <div className="App">hello world</div>;
 }
 
